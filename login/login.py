4f1a0af3ac718438501c73d4dbbb84743d732d30
Eduard Khiaev <eddietheone1984@gmail.com>
2023-06-14 08:12:24 +0000

    return session data

--- login/login.py
+++ login/login.py
@@ -1,31 +1,35 @@
 import requests
-import pprint
 from bs4 import BeautifulSoup
 
-# make a GET request to the login page
-URL = "http://192.168.1.165:8080/login.php"
-session = requests.Session()
-response = session.get(URL)
 
-# parse the HTML response to find the CSRF token
-soup = BeautifulSoup(response.text, 'html.parser')
-csrf_token = soup.find('input', attrs={'name': 'user_token'})['value']
+def login(url, username, password):
+    # make a GET request to the login page
+    session = requests.Session()
+    response = session.get(url)
 
-# now make a POST request to login, including the CSRF token
-login_data = {
-    'username': 'admin',
-    'password': 'password',
-    'Login': 'Login',
-    # make sure the csrf_token is mapped to 'user_token', not 'csrf_token'
-    'user_token': csrf_token,
-}
-response = session.post(URL, data=login_data, allow_redirects=False)
+    # parse the HTML response to find the CSRF token
+    soup = BeautifulSoup(response.text, "html.parser")
+    csrf_token = soup.find("input", attrs={"name": "user_token"})["value"]
 
-# create a dictionary of the response
-response_dict = {
-    'status_code': response.status_code,
-    'headers': dict(response.headers),
-    'body': response.text
-}
+    # now make a POST request to login, including the CSRF token
+    login_data = {
+        "username": username,
+        "password": password,
+        "Login": "Login",
+        "user_token": csrf_token,
+    }
+    response = session.post(url, data=login_data, allow_redirects=False)
 
-pprint.pprint(response_dict)
+    # convert the cookie jar to a dictionary
+    cookies_dict = {}
+    for cookie in session.cookies:
+        cookies_dict[cookie.name] = cookie.value
+
+    # return the cookies and the CSRF token
+    return cookies_dict, csrf_token
+
+
+cookies, csrf_token = login(
+    "http://10.200.13.100:8080/login.php", "admin", "password")
+print(cookies)
+print(csrf_token)
